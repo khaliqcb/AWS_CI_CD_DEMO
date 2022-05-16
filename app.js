395551a88aee2cc3f5749ddca1c252e3f347401b
Muhammad Khaliq ur Rehman <khaliq@citrusbits.com>
2022-05-16 17:13:30 +0500

    Initial Implementation

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+// Index route
+app.get('*', (req, res) => {
+  res.send("Hello World!")
+});
+
+// Start server
+const server = app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
+
+module.exports = server;
