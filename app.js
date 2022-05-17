2dd4519d34fc3c96b1532f2c6f415954e0dc240e
Muhammad Khaliq ur Rehman <khaliq@citrusbits.com>
2022-05-17 17:57:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/health', (req, res) => {
-    res.send("Health Check")
+app.get('/status', (req, res) => {
+    res.send("Status Check!")
 });
-
-app.get('/health1', (req, res) => {
-    res.send("Health1 Check")
-});
-
-app.get('/health2', (req, res) => {
-    res.send("Health2 Check")
-});
-
-app.get('/health3', (req, res) => {
-    res.send("Health3 Check")
-});
-
-// Index route
-// app.get('*', (req, res) => {
-//   res.send("Hello World!")
-// });
 
 // Start server
 const server = app.listen(port, () => {
