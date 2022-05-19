6f7213bedaaa21ab4ad8adea04b0d2734ad051a3
Muhammad Khaliq ur Rehman <khaliq@citrusbits.com>
2022-05-19 12:40:53 +0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,10 @@
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/express-app"
+
 DIR1="/home/ubuntu/express-app1"
 mkdir ${DIR1}
+
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
