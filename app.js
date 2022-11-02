6d13de44ef11a4bbfe847f3323709b0923970a08
Ahmad Quraishi <ahmadquraishi399@gmail.com>
2022-11-02 12:04:20 +0500

    Token Removed

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.get('/status', (req, res) => {
 });
 
 app.get('/ENV', (req, res) => {
-  //token Dev
   let PORT = process.env.PORT;
   let NAME = process.env.MY_NAME;
   let DEPLOYMENTGROUPNAME = process.env.DEPLOYMENT_GROUP_NAME;
